89b0a660885dd6ff7efc7f44b009bad089039abf
Luis <m.luishernandez@outlook.com>
2020-09-26 15:09:08 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diamond_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
