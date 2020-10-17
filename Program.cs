d5f240ec4d949cf4e485e311daa70fd77592f504
Luis <m.luishernandez@outlook.com>
2020-10-17 14:33:02 -0400

    Finished Diamond Display

--- Program.cs
+++ Program.cs
@@ -6,7 +6,46 @@ namespace Diamond_App
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+              
+               var star = 5;
+
+
+               for (var line=1; line<=star; line++)
+               {
+                   Console.WriteLine("");
+
+                   for(var space=line; space<=5; space++)
+                   {
+                       Console.Write("");
+                       Console.Write(" ");
+                       
+                   }
+
+                   for(var num=line; num>0; num--)
+                   {
+                       Console.Write(" ");
+                       Console.Write("*");
+                   }
+                   
+               }
+        
+                //Bottom Half of the diamond
+               
+              for (var line = 1; line<=star; line++)
+               {
+                   Console.WriteLine("");
+                   {
+                       for (var space = line; space>=star-4; space--)
+                       Console.Write(" ");
+                   }
+                   for (var num = line; num<=star; num++){
+                        Console.Write(" ");
+                        Console.Write("*");
+                   }
+                  
+               }
+              
         }
     }
 }
+
